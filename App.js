ab1c9cbe09bd4d77f18c530be2821941963f2b90
RohitGupta1235 <94480941+RohitGupta1235@users.noreply.github.com>
2022-09-01 23:41:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "./components/navbar";
+import Body from "./components/body";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Services from "./components/services";
+
+function App() {
+
+
+  return (
+    <>
+    
+
+    <Navbar/>
+    <Body/>
+    <Services/>
+    </>
+  );
+};
+
+export default App;
